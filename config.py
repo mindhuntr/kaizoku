f3ecff420570727846f84bf734f509c3e6642593
Irfan Jalal <mindhunter@blinkenshell.org>
2022-07-14 09:34:20 +0530

    added parameter to import cookies from browser

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import sys
 import os 
 import configparser 
+import browser_cookie3 
 
 parser = configparser.ConfigParser(interpolation=None) 
 fullpath = os.path.expanduser("~/.config/kaizoku.conf") 
@@ -12,7 +13,8 @@ def parse_conf():
         session_id = parser.get('kaizoku', 'session_id') 
 
         return session_id 
-
+    
+    print("Manually enter session id or import using --cookies-from-browser") 
     session_id = input("Enter instagram session id: ") 
 
     if session_id: 
@@ -24,3 +26,31 @@ def parse_conf():
         parser.write(f) 
 
     return session_id 
+
+def get_cookies(browser):
+    
+    if browser == "chrome": 
+        cookies = browser_cookie3.chrome(domain_name='.instagram.com')
+    
+    elif browser == "vivaldi": 
+        cookies = browser_cookie3.vivaldi(domain_name='.instagram.com')
+
+    elif browser == "firefox": 
+        cookies = browser_cookie3.firefox(domain_name='.instagram.com')
+        
+        session_id = "" 
+
+        for cookie in cookies:
+            if cookie.name == "sessionid":
+                session_id = cookie.value
+                
+        if session_id: 
+            parser['kaizoku'] = { 
+                    'session_id': session_id,
+                    }
+
+        with open(fullpath,'w') as f: 
+            parser.write(f) 
+
+        return session_id 
+
